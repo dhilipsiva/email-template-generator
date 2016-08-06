3c044ab758d6293c31f593f0a57640db3ecd1929
dhilipsiva <dhilipsiva@gmail.com>
2016-08-06 17:17:48 +0530

    Read from & Write to files

--- index.js
+++ index.js
@@ -9,43 +9,67 @@
 
   var sass = require('node-sass'),
     pug = require('pug'),
+    fs = require('fs'),
     juice = require('juice'),
     cheerio = require('cheerio'),
     Inky = require('inky').Inky,
     minify = require('html-minifier').minify,
-    styles, html, inlinedHtml, $, result, css, inky, options, minifiedHtml;
+    sourceDir = "source/",
+    distDir = "dist/",
+    scssDir = sourceDir + "scss/",
+    scssMain = scssDir + "main.scss",
+    pugDir = sourceDir + "pug/",
+    privateDir = pugDir + "private/",
+    styles, html, inlinedHtml, $, result, css, inky, minifiedHtml, files, file,
+    i, len, srcPath, targetPath;
 
-  options = {};
 
   // Render SCSS to CSS
   styles = sass.renderSync({
-    file: 'source/scss/main.scss',
+    file: scssMain,
     outputStyle: 'compressed',
   });
   css = styles.css.toString();
 
-  // Render PUG to HTML
-  html = pug.renderFile('source/pug/first.pug');
+  // List files
+  files = fs.readdirSync(privateDir);
+  for (i = 0, len = files.length; i < len; i++) {
+    file = files[i];
+    if (!file.endsWith(".pug")) {
+      continue;
+    }
+    srcPath = privateDir + file;
+    targetPath = distDir + file + ".html";
+    // Render PUG to HTML
+    html = pug.renderFile(srcPath);
 
-  // Apply Inky on Previous HTML
-  $ = cheerio.load(html);
-  inky = new Inky();
-  html = inky.releaseTheKraken($);
+    // Apply Inky on Previous HTML
+    $ = cheerio.load(html);
+    inky = new Inky();
+    html = inky.releaseTheKraken($);
 
-  // Apply the Styles Inline
-  inlinedHtml = juice(html, {extraCss: css});
+    // Apply the Styles Inline
+    inlinedHtml = juice(html, {extraCss: css});
 
-  // Add Additional attributes to HTML
-  $ = cheerio.load(inlinedHtml);
-  $('table').attr("border", "0");
-  $('table').attr("cellpadding", "0");
-  $('table').attr("cellspacing", "0");
-  $('a').attr("target", "_blank");
+    // Add Additional attributes to HTML
+    $ = cheerio.load(inlinedHtml);
+    $('table').attr("border", "0");
+    $('table').attr("cellpadding", "0");
+    $('table').attr("cellspacing", "0");
+    $('a').attr("target", "_blank");
 
-  // Save the File
-  minifiedHtml = minify($.html(), {
-    minifyCSS: true,
-    collapseWhitespace: true
-  });
-  console.log(minifiedHtml);
+    // Minify the File
+    minifiedHtml = minify($.html(), {
+      minifyCSS: true,
+      collapseWhitespace: true
+    });
+
+    // Save the file
+    fs.writeFile(targetPath, minifiedHtml, function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      console.log("Template generated: " + targetPath);
+    });
+  }
 })();
